3e463c910c047e7bec4ba3f00bf585fabe0b8c77
DilupaG <d.dilupa89@gmail.com>
2023-11-07 11:46:57 +0530

    feat: safe guard product website

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".accordion h2").forEach((header) => {
+  header.addEventListener("click", () => {
+    const content = header.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+});
